85dab6f40535036486be7daff01c3f5e3cb6fa0f
Felix Benning <felix.benning@gmail.com>
2024-09-04 10:33:19 +0200

    better tests

--- tests/test_matrices.py
+++ tests/test_matrices.py
@@ -1,13 +1,12 @@
 # pylint: disable=redefined-outer-name
 import pytest
-import scipy.sparse as sp
 import numpy as np
 
 from pygrf.matrices import KiteMatrix
 
 
 @pytest.fixture
-def random_kite_matrix_tuple(request):
+def random_kitematrix_tuple(request):
     """Return a random KiteMatrix"""
     rng = np.random.default_rng(request.param)
     rcol, joint, lrow = rng.integers(low=2, high=100, size=3)
@@ -26,10 +25,8 @@ def random_kite_matrix_tuple(request):
 
     return rmat, lmat
 
-
-@pytest.mark.parametrize("random_kite_matrix_tuple", range(100), indirect=True)
-def test_matmul(random_kite_matrix_tuple):
-    """Test matrix multiplication"""
+def test_kitematrix_matmul_handcrafted():
+    """Test matrix multiplication with handcrafted examples"""
     mat1 = KiteMatrix(dense=np.array([[1, 2], [3, 4]]), sparse=(1,2))
     mat2 = KiteMatrix(dense=np.array([[-1, 5], [6, 7]]), sparse=(3,2))
     assert (mat1.toarray() @ mat2.toarray() == (mat1 @ mat2).toarray()).all()
@@ -38,7 +35,18 @@ def test_matmul(random_kite_matrix_tuple):
     mat2 = KiteMatrix(dense=np.array([[-1, 5, 2], [6, 7, 2], [1,2,3]]), sparse=(3, 2))
     assert (mat1.toarray() @ mat2.toarray() == (mat1 @ mat2).toarray()).all()
 
-    matl, matr = random_kite_matrix_tuple
+@pytest.mark.parametrize("random_kitematrix_tuple", range(100), indirect=True)
+def test_kitematrix_matmul_random(random_kitematrix_tuple):
+    """Test matrix multiplication with random input"""
+    mat1 = KiteMatrix(dense=np.array([[1, 2], [3, 4]]), sparse=(1,2))
+    mat2 = KiteMatrix(dense=np.array([[-1, 5], [6, 7]]), sparse=(3,2))
+    assert (mat1.toarray() @ mat2.toarray() == (mat1 @ mat2).toarray()).all()
+
+    mat1 = KiteMatrix(dense=np.array([[1, 2], [3, 4]]), sparse=(1, 3))
+    mat2 = KiteMatrix(dense=np.array([[-1, 5, 2], [6, 7, 2], [1,2,3]]), sparse=(3, 2))
+    assert (mat1.toarray() @ mat2.toarray() == (mat1 @ mat2).toarray()).all()
+
+    matl, matr = random_kitematrix_tuple
     res1 = matl.toarray() @ matr.toarray()
     res2= (matl @ matr).toarray()
     assert np.linalg.matrix_norm(res1 - res2) == pytest.approx(0)
