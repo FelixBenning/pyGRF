fix kernel test and numerical stability

pygrf/kernels.py
@@ -133,7 +133,7 @@ class IsotropicKernel(Kernel):
             np.einsum("kl,ki,lj->ikjl", k_12, _c1, _c2) 
             + np.einsum("kl,ki,kj->ikjl", k_13, _c1, _c1)
             + np.einsum("kl,li,lj->ikjl", k_23, _c2, _c2)
-            + np.einsum("kl,kj,li->ikjl", k_33, _c1, _c2)
+            + np.einsum("kl,li,kj->ikjl", k_33, _c2, _c1)
         ) + np.einsum("kl,ij->ikjl", k_3, np.identity(basis_len))
 
         return result

tests/test_kernels.py
@@ -23,14 +23,14 @@ def test_squared_exponential(regtest):
     print(res, file=regtest)
 
 
-@pytest.mark.parametrize("seed", range(100))
+@pytest.mark.parametrize("seed", range(50))
 def test_squared_exponential_random(seed):
     rng = np.random.default_rng(seed)
-    c1_len = rng.integers(low=1, high=100)
-    c2_len = rng.integers(low=1, high=100)
-    dim = rng.integers(low=1, high=100)
-    c1 = rng.normal((c1_len, dim))
-    c2 = rng.normal((c2_len, dim))
+    c1_len = rng.integers(low=1, high=20)
+    c2_len = rng.integers(low=1, high=20)
+    dim = rng.integers(low=1, high=20)
+    c1 = rng.normal(size=(c1_len, dim))
+    c2 = rng.normal(size=(c2_len, dim))
 
     var = rng.normal() ** 2
     length_scale = rng.normal() ** 2
@@ -40,7 +40,7 @@ def test_squared_exponential_random(seed):
 
     autocov = k.covariance(c1, c1, derivatives=1)
     autocov = autocov.reshape(autocov.shape[0] * autocov.shape[1], -1)
-    assert sp.linalg.issymmetric(autocov)
+    assert np.allclose(autocov - autocov.T, 0)
 
     try:
         np.linalg.cholesky(autocov)